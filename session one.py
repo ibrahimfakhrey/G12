e9c4a628877c140223d51cdd7ce7cf73a13ec431
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-02-28 09:20:20 +0200

    Initial commit

---
+++ session one.py
@@ -0,0 +1,24 @@
+# print("hello world")
+# x = "3"
+# print(x)
+# change the variable into int
+# x = 3
+# print(x * 4)
+# take the input from the user
+# x=input("please enter your name ")
+# print("HI "+ x )
+# secret="ziad"
+# if x.lower() == secret.lower():
+#     print("hello ")
+# else:
+#     print("noo")
+names = ["sief", "ziad"]
+
+#adding an item to the list
+names.append("hamza")
+# lopping throw the list using for
+for i in names:
+    if i =="ziad":
+        print("zyad")
+    else:
+        print(i)
