4dc67206938c1a7b3c28f08e8b43ed15047a0f19
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-03-04 09:30:39 +0200

    added and finish lesson  four

---
+++ session 4.py
@@ -0,0 +1,26 @@
+#explain the while loop
+
+# n=0
+# on=True
+# while on :
+#     if n != 10:
+#       print(f"{n+1} ---this is the first while for me ")
+#       n+=1
+#     else:
+#         on=False
+
+
+# get the numbers using while
+
+# n= 10
+# while n !=0:
+#     print(n)
+#     n-=1
+
+# get the even and odd numbers
+# n=10
+# while n >=0:
+#     if n % 2==0:
+#         print(f"this is even number {n}")
+#     n-=1
+
