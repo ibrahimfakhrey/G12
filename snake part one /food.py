9e8ee8c5f8b293349cd5a67acfd1f54b132cff5f
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-03-18 14:26:56 +0200

    added and finish lesson  sssss

---
+++ snake part one /food.py
@@ -0,0 +1,8 @@
+# your task is to make the class of the food it should be a circle with a blue color
+# and it appears in a random place in the screen
+#and when the snake make colosion with the food it dis appear and appears in another place
+
+# ths snake if it touch any screen game stops
+
+class Food:
+    pass
--- snake part one /main.py
+++ snake part one /main.py
@@ -1,50 +1,29 @@
 import time
 from turtle import Turtle,Screen
+from snake import Snake
 
-def up():
-    if all_segments[0].heading() != 270:
-        all_segments[0].setheading(90)
-
-def down():
-    if all_segments[0].heading() != 90:
-        all_segments[0].setheading(270)
-def left():
-    if all_segments[0].heading() != 0:
-        all_segments[0].setheading(180)
-def right():
-    if all_segments[0].heading() != 180:
-        all_segments[0].setheading(0)
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My snake Game ")
 screen.tracer(0)
 screen.listen()
-screen.onkey(up,"Up")
-screen.onkey(down,"Down")
-screen.onkey(left,"Left")
-screen.onkey(right,"Right")
 
 
-positions=[(0,0),(-20,0),(-40,0)]
-all_segments=[]
-for p in positions:
-    new_seg=Turtle("square")
-    new_seg.penup()
-    new_seg.color("white")
-    new_seg.goto(p)
+snake=Snake()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-    all_segments.append(new_seg)
+game_is_on=True
+while game_is_on:
 
-while True:
-    time.sleep(0.1)
     screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-    for i in range (len(all_segments)-1,0,-1):
-        new_x=all_segments[i-1].xcor()
-        new_y=all_segments[i-1].ycor()
-        all_segments[i].goto(new_x,new_y)
-    all_segments[0].forward(10)
+
 
 
 
