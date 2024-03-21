cb351707d5685b58e57a4fdf89d2c82c17e3cba2
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-03-21 14:55:31 +0200

    added and finish lesson  sssssjjj

--- snake part one /food.py
+++ snake part one /food.py
@@ -3,6 +3,19 @@
 #and when the snake make colosion with the food it dis appear and appears in another place
 
 # ths snake if it touch any screen game stops
+from turtle import Turtle
+import random
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
 
-class Food:
-    pass
+    def refresh(self):
+        random_x=random.randint(-280,280)
+        random_y=random.randint(-280,280)
+        self.goto(random_x,random_y)
--- snake part one /main.py
+++ snake part one /main.py
@@ -1,16 +1,19 @@
 import time
 from turtle import Turtle,Screen
 from snake import Snake
-
+from food import Food
+from scoreboard import Scoreboard
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My snake Game ")
 screen.tracer(0)
 screen.listen()
+scoreboard=Scoreboard()
 
 
 snake=Snake()
+food=Food()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -20,8 +23,15 @@ game_is_on=True
 while game_is_on:
 
     screen.update()
-    time.sleep(0.1)
+    time.sleep(1)
     snake.move()
+    if snake.head.distance(food)<15:
+        food.refresh()
+        scoreboard.increase_score()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+
+
 
 
 
